30626b15776392cdcc25b6082367e5f72cf18d33
rdziaczynski12 <rdziaczynski@gmail.com>
2018-12-19 08:18:15 +0100

    add postgrSQL database

--- back-end/src/main/resources/import.sql
+++ back-end/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO dbo.db_user (name, PASSWORD) VALUES('Jan', 'qwe');
-INSERT INTO dbo.db_user (name, PASSWORD) VALUES('Marcin', 'asd');
-INSERT INTO dbo.db_user (name, PASSWORD) VALUES('Jacek', 'zxc')
-INSERT INTO dbo.db_user (name, PASSWORD) VALUES('Aneta', '123');
-INSERT INTO dbo.db_user (name, PASSWORD) VALUES('Kuba', '456');
+INSERT INTO db_user (name, PASSWORD) VALUES('Jan', 'qwe');
+INSERT INTO db_user (name, PASSWORD) VALUES('Marcin', 'asd');
+INSERT INTO db_user (name, PASSWORD) VALUES('Jacek', 'zxc')
+INSERT INTO db_user (name, PASSWORD) VALUES('Aneta', '123');
+INSERT INTO db_user (name, PASSWORD) VALUES('Kuba', '456');
